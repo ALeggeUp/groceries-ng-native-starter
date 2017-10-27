06482da615dbb5854a73227490033db3d7b7afea
Stephen Legge <stephen@aleggeup.com>
2017-10-27 01:09:55 -0400

    Styles specific to the login page

--- app/app.component.ts
+++ app/app.component.ts
@@ -11,7 +11,8 @@ import { Component } from "@angular/core";
       <Button text="Sign in"></Button>
       <Button text="Sign up for Groceries"></Button>
     </StackLayout>
-  `
+  `,
+  styleUrls: ["pages/login/login-common.css", "pages/login/login.css"]
 })
 
 export class AppComponent {}
