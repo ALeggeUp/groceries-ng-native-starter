53febb932716f4548cbce3cad9fd2e823d075dbb
Stephen Legge <stephen@aleggeup.com>
2017-10-29 03:10:40 -0400

    Correct the text colour on the sign up background and make the status bar translucent

--- app/pages/login/login.component.ts
+++ app/pages/login/login.component.ts
@@ -4,9 +4,11 @@ import { Router } from "@angular/router";
 import { Color } from "color";
 import { Page } from "ui/page";
 import { View } from "ui/core/view";
+import { TextField } from "ui/text-field";
 
 import { User } from "../../shared/user/user";
 import { UserService } from "../../shared/user/user.service";
+import { setHintColor } from "../../utils/hint-util";
 
 @Component({
   selector: "my-app",
@@ -22,6 +24,8 @@ export class LoginComponent implements OnInit {
   isLoggingIn = true;
 
   @ViewChild("container") container: ElementRef;
+  @ViewChild("email") email: ElementRef;
+  @ViewChild("password") password: ElementRef;
 
   constructor(private router: Router, private userService: UserService, private page: Page) {
     this.user = new User();
@@ -42,6 +46,8 @@ export class LoginComponent implements OnInit {
 
   toggleDisplay() {
     this.isLoggingIn = !this.isLoggingIn;
+    this.setTextFieldColors();
+
     let container = <View>this.container.nativeElement;
     container.animate({
       backgroundColor: this.isLoggingIn ? new Color("white") : new Color("#301217"),
@@ -66,6 +72,19 @@ export class LoginComponent implements OnInit {
         },
         () => alert("Unfortunately we were unable to create your account.")
       );
+  }
+
+  setTextFieldColors() {
+    let emailTextField = <TextField>this.email.nativeElement;
+    let passwordTextField = <TextField>this.password.nativeElement;
+
+    let mainTextColor = new Color(this.isLoggingIn ? "black" : "#C4AFB4");
+    emailTextField.color = mainTextColor;
+    passwordTextField.color = mainTextColor;
+
+    let hintColor = new Color(this.isLoggingIn ? "#ACA6A7" : "#C4AFB4");
+    setHintColor({ view: emailTextField, color: hintColor });
+    setHintColor({ view: passwordTextField, color: hintColor });
   }
 
   ngOnInit() {
